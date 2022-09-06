4750eceac20be4cb40a06e111f0d59604a804859
rondymesquita <rondinellimesquitas@gmail.com>
2022-09-06 15:36:42 -0300

    feat: prepare base url vite for gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteVConsole } from "vite-plugin-vconsole";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteVConsole } from 'vite-plugin-vconsole';
+import * as path from 'path';
+
+export const base =
+  process.env.NODE_ENV === 'development'
+    ? undefined
+    : '/the-agile-code-review-manifesto/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 });

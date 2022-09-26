20b66c01d3996d580c43043a31a67e329b56bf41
rondymesquita <rondinellimesquitas@gmail.com>
2022-09-26 14:43:53 -0300

    feat: add api swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Canvas API')
+    .setDescription('The canvas API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();

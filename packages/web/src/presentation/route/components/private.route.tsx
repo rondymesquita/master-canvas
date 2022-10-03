07269f51eb9f13d0c52a3e2ee3975aba51cc8bd7
rondymesquita <rondinellimesquitas@gmail.com>
2022-10-03 13:21:41 -0300

    chore: rename user state hook

--- packages/web/src/presentation/route/components/private.route.tsx
+++ packages/web/src/presentation/route/components/private.route.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import useLocalUser from '../../../app/usecase/user/useLocalUser';
+import useLocalUser from '../../../app/usecase/user/useUser';
 import { LOGIN_PAGE } from '../routes';
 
 export default function PrivateRoute({ children, ...rest }: any) {

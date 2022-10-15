d9221499faa25128da5cb6097337b160a2c19997
rondymesquita <rondinellimesquitas@gmail.com>
2022-10-15 19:07:28 -0300

    chore: set all button rounded

--- packages/web/src/presentation/theme/theme.ts
+++ packages/web/src/presentation/theme/theme.ts
@@ -1,4 +1,10 @@
-import { extendTheme } from '@chakra-ui/react';
+import {
+  DeepPartial,
+  extendTheme,
+  Theme,
+  ThemeExtension,
+  ThemeOverride,
+} from '@chakra-ui/react';
 import { colors } from './colors';
 
 const fontFamily = 'Chivo, Inter, Avenir, Helvetica, Arial, sans-serif';
@@ -29,20 +35,21 @@ export const theme = extendTheme({
       },
     },
     Button: {
-      // baseStyle: {
-      //   boxShadow:
-      //     '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      //   '&:hover': {
-      //     boxShadow:
-      //       '0 10px 15px -3px rgb(0 0 0 / 10%), 0 15px 15px -2px rgb(0 0 0 / 10%)',
-      //     transform: 'scale(1.03)',
-      //   },
-      // },
+      baseStyle: {
+        borderRadius: 'full',
+        // boxShadow:
+        //   '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        // '&:hover': {
+        //   boxShadow:
+        //     '0 10px 15px -3px rgb(0 0 0 / 10%), 0 15px 15px -2px rgb(0 0 0 / 10%)',
+        //   transform: 'scale(1.03)',
+        // },
+      },
     },
   },
   colors: {
     primary: defaultTheme.colors.purple,
-    // primary: colors.violet,
+    // primary: colors.plum,
     accent: colors.plum,
     destructive: colors.plum,
     background: colors.slate,
